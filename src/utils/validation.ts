77a2ee5ee236bf828ee38c151c4ae1fe18945f76
althafdemiandra <92288572+althafdaa@users.noreply.github.com>
2022-08-25 21:04:57 +0700

    feat: create register page

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,3 +4,9 @@ export const LoginValidationSchema = y.object().shape({
   email: y.string().email().required('Email is required'),
   password: y.string().required('Password is required'),
 });
+
+export const RegisterValidationSchema = y.object().shape({
+  name: y.string().email().required('Name is required'),
+  email: y.string().email().required('Email is required'),
+  password: y.string().required('Password is required'),
+});
